0362b433a40bf61df3f2f91d08ac9a54924e1340
Crystian Luis <crystianluis@gmail.com>
2021-04-07 22:12:52 -0300

    add .

---
+++ exemplo-03.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = "Hcode";
+$site = 'www.hcode.com.br';
+
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+
+////////////////////
+
+$frutas = array("abacaxi", "laranja", "manga");
+
+echo $frutas[2];
+
+?>
