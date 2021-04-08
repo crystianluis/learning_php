3ebe134871e06992efb82fc53dc867b2d5bb9dcd
Crystian Luis <crystianluis@gmail.com>
2021-04-07 22:21:22 -0300

    arquvo 03

--- exemplo-03.php
+++ exemplo-03.php
@@ -7,10 +7,23 @@ $ano = 1990;
 $salario = 5500.99;
 $bloqueado = false;
 
-////////////////////
+///////////////////////////////////////////////
 
 $frutas = array("abacaxi", "laranja", "manga");
 
-echo $frutas[2];
+//echo $frutas[2];
+
+$nascimento = new DateTime();
+
+//var_dump($nascimento);
+
+///////////////////////////////////////////////
+
+$arquivo = fopen("exemplo-03.php", "r")
+
+//var_dump($arquivo);
+
+$nulo = NULL;
+$vazio = "";
 
 ?>
