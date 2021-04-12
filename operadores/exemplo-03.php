80b57b98cd83b8369521dcbb510bc09741ad2b72
Crystian Luis <crystianluis@gmail.com>
2021-04-12 20:23:58 -0300

    teste

---
+++ operadores/exemplo-03.php
@@ -0,0 +1,34 @@
+<?php
+
+//Operadores Aritméticos
+
+$a = 10;
+$b = 2;
+
+echo $a + $b;
+
+echo "<br>";
+
+echo $a - $b;
+
+echo "<br>";
+
+echo $a * $b;
+
+echo "<br>";
+
+echo $a / $b;
+
+echo "<br>";
+
+//modulo
+echo $a % $b;
+
+echo "<br>";
+
+//elevado ao quadrado
+echo $a ** $b;
+
+echo "<br>";
+
+?>
