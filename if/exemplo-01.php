095a8c862ba102cf3bbb66cb6fa548044958766a
Crystian Luis <crystianluis@gmail.com>
2021-04-13 11:01:19 -0300

    teste

---
+++ if/exemplo-01.php
@@ -0,0 +1,18 @@
+<?php
+
+$suaIdade = 30;
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeIdoso = 65;
+
+if ($suaIdade < $idadeCrianca ) {
+    echo "você é uma criança!";
+} else if ( $suaIdade >= $idadeCrianca && $suaIdade < $idadeMaior ) {
+    echo "Você é um adolescente!";
+} else if ( $suaIdade >= $idadeMaior && $suaIdade < $idadeIdoso ) {
+    echo "Você é um adulto!";
+} else {
+    echo "Você é um idoso!";
+}
+
+?>
