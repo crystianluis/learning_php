942b77ab8e888edfa9fd0b073e9a64f208f90e67
Crystian Luis <crystianluis@gmail.com>
2021-04-14 10:49:03 -0300

    teste

--- switch/exemplo-01.php
+++ switch/exemplo-01.php
@@ -2,7 +2,29 @@
 
 $diaSemana = date("w");
 
-echo $diaSemana;
+switch ($diaSemana) {
 
+    case 0:
+        echo "Domingo";
+        break;
+    case 1:
+        echo "Segunda-feira";
+        break;
+    case 2:
+        echo "Terça-feira";
+        break;
+    case 3:
+        echo "Quarta-feira";
+        break;
+    case 4:
+        echo "Quinta-feira";
+        break;
+    case 5:
+        echo "Sexta-feira";
+        break;
+    case 6:
+        echo "Sábado";
+        break;
+}
 
 ?>
