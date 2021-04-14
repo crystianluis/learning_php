e22f245577b82896c43cf0a44d7e7bacbffcc877
Crystian Luis <crystianluis@gmail.com>
2021-04-14 14:29:52 -0300

    teste

---
+++ constante/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+
+define("SERVIDOR", "127.0.0.1");
+define("BANCO_DE_DADOS", [
+    '127.0.0.1',
+    'root',
+    'password',
+    'test'
+]);
+
+echo SERVIDOR;
+print_r(BANCO_DE_DADOS);
+
+?>
