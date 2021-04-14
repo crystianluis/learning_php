7efabefb87f3e483cb0c773a1a11b339d08c2cb7
Crystian Luis <crystianluis@gmail.com>
2021-04-14 11:28:46 -0300

    teste

---
+++ for/exemplo-03.php
@@ -0,0 +1,22 @@
+<?php
+
+<form>
+
+    <input type="text" name="name">
+    <input type="date" name="date">
+    <input type="submit" value="OK">
+
+</form>
+
+if (isset($_GET)) {
+
+    foreach ($_GET as $key => $value) {
+        
+        echo "Nome do campo: " . $$key;
+        echo "Valor do Campo: " . $value; 
+        echo "<hr>";
+    }
+
+}
+
+?>
